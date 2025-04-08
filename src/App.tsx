769abc30111edd64be9cb1a99ee7decf63a53852
jiseong1688 <wltjd1688@naver.com>
2025-04-08 16:11:50 +0900

    feat: 클래스형 컴포넌트와 함수형 컴포넌트

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import ClassCom from './ClassCom';
+import FuncCom from './FuncCom';
 
 {/*
   작성자: kjm
@@ -10,23 +12,25 @@ import './App.css';
 function App(){
   let name = "리액트";
   const style = {
-    backgroundColor: 'balck',
-    color: 'white',
-    fontSize: '48px',
-    fontWeigth: 'bold',
-    padding: '20px'
+    background: "blueviolet",
+    color: "white",
+    fontSize: "48px",
+    fontWeight: "bold",
+    padding: "20px"
   };
 
   return (
     <>
-      <div className="container" >
-        <h1 style = {style} >Hello
+      <div className="containel" >
+        {/* <h1 style = {style} >Hello
           {
             name === "리액트" ? (<h1>YES</h1>): null
           }!!
         </h1>
-        {/* <p>반갑습니다.</p> */}
-        <br/>
+        // {/* <p>반갑습니다.</p> 
+        <br/> */}
+        <ClassCom/>
+        <FuncCom/>
       </div>
     </>
   )
